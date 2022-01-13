95644185b9f50447c3e2ce46e29d62fb9eca3889
Sulaiman Haque <37465938+dimiksonkha@users.noreply.github.com>
2022-01-13 17:15:06 +0600

    Update linear_search.py

--- searching/linear_search.py
+++ searching/linear_search.py
@@ -5,10 +5,7 @@ Python implementation of linear search
 def linear_search(item_list, item):
     n = len(item_list)
     for i in range(n):
-        print("Looping!")
         if item == item_list[i]:
             return i
-            
-
     return -1
 
